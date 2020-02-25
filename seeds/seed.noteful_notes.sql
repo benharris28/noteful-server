ff9abc91ea53e7446932064979af29bd3c170067
Ben Harris <benjaminharris28@gmail.com>
2020-02-25 13:57:44 -0500

    added folders router and service obj

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,7 @@
+INSERT INTO noteful_notes (id, name, folderId, content)
+VALUES
+(1, 'Dogs', 1, 'Bla bla bla'),
+(2, 'Hello', 2, 'Hello note'),
+(3, 'Another note', 3, 'Yes this is another note'),
+(4, 'Additional note', 3, 'Additional fun'),
+(5, 'Stuff', 2, 'This should be enough for now');
